e4fdb96b446ff7e0455c2a65bf845b474a523c86
Justin Schneck <jschneck@mac.com>
2019-01-30 15:38:02 -0500

    Step 5 - Version 0.2.0!

--- nerves_team_device/mix.exs
+++ nerves_team_device/mix.exs
@@ -6,7 +6,7 @@ defmodule NervesTeamDevice.MixProject do
   def project do
     [
       app: :nerves_team_device,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.8",
       archives: [nerves_bootstrap: "~> 1.4"],
       start_permanent: Mix.env() == :prod,
